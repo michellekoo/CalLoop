dccf4fb736cd9c681618c41ae81b71e477df39d6
Alina <alinavak@berkeley.edu>
2015-10-03 13:39:31 -0700

    Adding models

--- CalLoop/CalCalendar/models.py
+++ CalLoop/CalCalendar/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+from django.db.models import *
 
 # Create your models here.
+class Subject(Model):
+	name = CharField()
+	weekday = ManyToManyField(Day)
+
+
+class Assignment(Model):
+	name = CharField()
+	due_date = DateTimeField()
+	due_in = DurationField()
+	subject = ForeignKey(Subject)
+
+class Day(Model):
+	name = CharField(choices=(('Monday'), ('Tuesday'), ('Wednesday'),
+		    ('Thursday'),) ('Friday'), ('Saturday'), ('Sunday'))
