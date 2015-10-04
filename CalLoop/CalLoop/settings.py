b0badd160730e850bfef0addf5fee28ac953fe18
Alina <alinavak@berkeley.edu>
2015-10-03 17:19:11 -0700

    Added static stuff

--- CalLoop/CalLoop/settings.py
+++ CalLoop/CalLoop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'CalCalendar',
+    'CalLoop',
 )
 
 MIDDLEWARE_CLASSES = (
--- CalLoop/CalLoop/urls.py
+++ CalLoop/CalLoop/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from CalCalendar import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^user_home/$', views.user_home),
     url(r'^login/$', views.user_auth),
-    url(r'^$', views.index)
-]
+    url(r'^$', views.index),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
